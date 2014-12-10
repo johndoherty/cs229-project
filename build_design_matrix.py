7cd4fbc3d136f8f5930e43eac4f228c3cc733276
John Doherty <john.doherty13@gmail.com>
2014-12-10 11:28:16 -0800

    online learning, position in prob

--- build_design_matrix.py
+++ build_design_matrix.py
@@ -6,6 +6,7 @@ import numpy as np
 
 out_filename = "design_matrix.npy"
 data_filename = "data.npy"
+cluster_to_index = {}
 
 def gpx_from_design_matrix(design_matrix, labels, centroids, f_name):
     # Output clusters
@@ -30,23 +31,42 @@ def gpx_from_design_matrix(design_matrix, labels, centroids, f_name):
     with open(f_name, 'w') as out:
         out.write(gpx.to_xml())
 
+def build_cluster_to_index_map(data):
+    clusters = set()
+    for point in data:
+        clusters.add(point['cluster'])
+    clusters = sorted(list(clusters))
+
+    index = 0
+    for point in data:
+        if not point['cluster'] in cluster_to_index:
+            cluster_to_index[point['cluster']] = index
+            index += 1
+
 def extract_features(curr_point, previous_point):
     """
     points are given as dicts with keys (arrival_time, departure_time, lat, lng, cluster)
     """
-    current_cluster = curr_point['cluster']
+    current_cluster = [0] * len(cluster_to_index)
+    previous_cluster = [0] * len(cluster_to_index)
+    current_cluster[cluster_to_index[curr_point['cluster']]] = 1
+    previous_cluster[cluster_to_index[previous_point['cluster']]] = 1
+
     datetime = dateutil.parser.parse(curr_point['departure_time'])
-    day_of_week = datetime.weekday()
-    is_weekend = (day_of_week == 5 or day_of_week == 6)
-    hour_3 = datetime.time().hour / 3
-    ispm = datetime.time().hour >= 12
-    mwf = (day_of_week == 0 or day_of_week == 2 or day_of_week == 4)
+    day_of_week = [0] * 7
+    day_of_week[datetime.weekday()] = 1
 
-    previous_cluster = previous_point['cluster']
+    hour_bin_size = 6
+    hour_bin = [0] * (24 / hour_bin_size)
+    hour_bin[datetime.time().hour / hour_bin_size] = 1
 
-    return [current_cluster, previous_cluster, day_of_week, is_weekend, hour_3, ispm]
-    #return [current_cluster, day_of_week, is_weekend, hour_3, ispm]
-    #return [current_cluster, day_of_week, hour_3]
+    is_weekend = 1 if (day_of_week == 5 or day_of_week == 6) else 0
+    ispm = 1 if datetime.time().hour >= 12 else 0
+    mwf = 1 if (day_of_week == 0 or day_of_week == 2 or day_of_week == 4) else 0
+
+    #features = day_of_week + hour_bin + [is_weekend, ispm]
+    features = current_cluster + day_of_week + hour_bin + [is_weekend, ispm]
+    return features
 
 def build_design_matrix(data):
     X = []
@@ -54,6 +74,8 @@ def build_design_matrix(data):
     for i in range(1, len(data) - 1):
         if data[i]['cluster'] == data[i+1]['cluster'] or data[i]['cluster'] == -1 or data[i+1]['cluster'] == -1:
             continue
+        # Only instances when I am leaving home
+        #if data[i]['cluster'] == 2:
         X.append(extract_features(data[i], data[i-1]))
         Y.append(data[i+1]['cluster'])
 
@@ -76,9 +98,11 @@ if __name__ == "__main__":
     with open(args.data, 'r') as data_file:
         data = json.load(data_file)
 
+    build_cluster_to_index_map(data)
     design_matrix, labels = build_design_matrix(data)
 
     out_filename = args.design
+    print "Design matrix shape: {0}, {1}".format(design_matrix.shape[0], design_matrix.shape[1])
     with open(out_filename, 'w') as outfile:
         np.save(outfile, design_matrix)
         print "Wrote design matrix to {0}".format(out_filename)
--- train_and_test.py
+++ train_and_test.py
@@ -1,25 +1,27 @@
 import argparse
 import numpy as np
+import matplotlib.pyplot as plt
 from sklearn import cross_validation
 from sklearn import svm
 from sklearn.multiclass import OneVsRestClassifier
 from sklearn.linear_model import LogisticRegression
+from sklearn.lda import LDA
 from sklearn.ensemble import RandomForestClassifier
 from sklearn.metrics import confusion_matrix
-from matplotlib import pyplot
+from sklearn.metrics import f1_score
 
-MODELS = ["SVM", "LogisticRegression", "RandomForest"]
+MODELS = ["SVM", "LogisticRegression", "RandomForest", "LDA"]
 
-def train(X, y, model):
-    if model == "SVM":
-        clf = svm.SVC(probability=True)
-        #clf = svm.SVC()
-        clf.fit(X,y)
-    elif model == "LogisticRegression":
-        clf = OneVsRestClassifier(LogisticRegression()).fit(X,y)
-    elif model == "RandomForest":
-        clf = RandomForestClassifier(50).fit(X,y)
-    return clf
+def get_model(model_name):
+    if model_name == "SVM":
+        return svm.SVC(probability=True, class_weight='auto')
+        #return OneVsRestClassifier(svm.LinearSVC(probability=True))
+    elif model_name == "LogisticRegression":
+        return OneVsRestClassifier(LogisticRegression())
+    elif model_name == "RandomForest":
+        return RandomForestClassifier(50)
+    elif model_name == "LDA":
+        return LDA()
 
 def predict(clf, x, classes, use_prob = False):
     if use_prob:
@@ -42,12 +44,12 @@ def confusion_to_image(conf, output_file):
         count = np.sum(conf[row, :])
         if count > 0:
             normalize_conf[row, :] = conf[row, :] / float(count)
-    figure = pyplot.figure()
-    pyplot.clf()
+    figure = plt.figure()
+    plt.clf()
     ax = figure.add_subplot(111)
     ax.set_aspect(1)
-    res = ax.imshow(np.array(normalize_conf), cmap=pyplot.cm.jet, interpolation='nearest')
-    pyplot.savefig(output_file, format='png')
+    res = ax.imshow(np.array(normalize_conf), cmap=plt.cm.jet, interpolation='nearest')
+    plt.savefig(output_file, format='png')
 
 
 def test(clf, X, Y, labels):
@@ -64,6 +66,83 @@ def test(clf, X, Y, labels):
     score = float(correct) / len(Y)
     return score
 
+def precision_recall_from_confusion(confusion_matrix):
+    true_count = np.sum(confusion_matrix, 1)
+    predicted_count = np.sum(confusion_matrix, 0)
+    correct = np.diagonal(confusion_matrix)
+    true_count = true_count.astype(float)
+    predicted_count = predicted_count.astype(float)
+    correct = correct.astype(float)
+    print "True count", true_count
+    print "Predicted count", predicted_count
+    print "Correct", correct
+    precision = np.divide(correct, predicted_count)
+    recall = np.divide(correct, true_count)
+    return precision, recall
+
+def evaluate_online(errors, actual, predicted, locations):
+    f1 = f1_score(actual, predicted, average=None)
+    confusion = confusion_matrix(actual, predicted)
+    precision, recall = precision_recall_from_confusion(confusion)
+    window_error = np.convolve(errors.astype(float), (np.ones(8)/8))
+    window_location = np.convolve(locations.astype(float), (np.ones(5)/5))
+    print "Online confusion", confusion
+    print "F1 score", f1
+    print "Precision", precision
+    print "Recall", recall
+    plt.plot(window_location)
+    #plt.plot(locations)
+    #plt.axis([0, 200, -1, 2])
+    plt.show()
+
+
+def location_in_predicted(model, X, y):
+    class_to_index = {c: i for i, c in enumerate(model.classes_)}
+    probs = model.predict_proba(X)
+    locations = []
+    for row in range(X.shape[0]):
+        p = probs[row, :]
+        sorted_indexes = np.argsort(p)[::-1]
+        loc = np.where(sorted_indexes == class_to_index[y[row]])
+        locations.append(loc[0][0])
+    print "Mean position of correct class", np.mean(np.array(locations))
+    print locations
+    print zip(locations, y.tolist())
+    plt.hist(locations)
+    #plt.axis([0, 200, -1, 2])
+    plt.show()
+    return locations
+
+
+def errors_by_features(X, y_predicted, y_actual):
+    return
+
+
+def online_learning(model, X, y, min_data):
+    classes = set(y)
+    class_to_index = {c: i for i, c in enumerate(classes)}
+    errors = []
+    predicted = []
+    actual = []
+    locations = []
+    for i in range(min_data, X.shape[0]):
+        model.fit(X[:i-1, :], y[:i-1])
+
+        probs = model.predict_proba(X[i, :])[0]
+        expanded_probs = [0] * len(classes)
+        for j, p in enumerate(probs):
+            c = model.classes_[j]
+            expanded_probs[class_to_index[c]] = p
+        sorted_indexes = np.argsort(expanded_probs)[::-1]
+        loc = np.where(sorted_indexes == class_to_index[y[i]])
+        locations.append(loc[0][0])
+
+        prediction = model.predict(X[i, :])
+        actual.append(y[i])
+        predicted.append(prediction)
+        errors.append(0 if prediction == y[i] else 1)
+    return np.array(actual), np.array(predicted), np.array(errors), np.array(locations)
+    
 
 if __name__ == "__main__":
     parser = argparse.ArgumentParser(description='Concatenate data files into a more usable format')
@@ -79,28 +158,40 @@ if __name__ == "__main__":
     with open(args.labels, 'r') as labels:
         y = np.load(labels)
 
-    #print cross_validation.cross_val_score(model, X, y, scoring='recall')
+    model = get_model(args.model)
+    actual, predicted, errors, locations = online_learning(model, X, y, 20)
+    evaluate_online(errors, actual, predicted, locations)
 
+    #print cross_validation.cross_val_score(model, X, y, scoring='recall')
     avg_score = 0
     n_folds = 2
-    if (True):
+    if (False):
         kf = cross_validation.StratifiedKFold(y, n_folds=n_folds)
         for train_index, test_index in kf:
             X_train, X_test = X[train_index], X[test_index]
             y_train, y_test = y[train_index], y[test_index]
-            clf = train(X_train, y_train, args.model)
+            model.fit(X_train, y_train)
             labels = set(y_train.tolist())
             sorted_labels = sorted(list(labels))
-            avg_score += test(clf, X_test, y_test, sorted_labels)
+            avg_score += test(model, X_test, y_test, sorted_labels)
         avg_score /= n_folds
         print avg_score
     else:
         X_train, X_test, y_train, y_test = cross_validation.train_test_split(X, y, test_size=.3, random_state=0)
-        clf = train(X_train, y_train, args.model)
-        predicted = clf.predict(X_test)
+        model.fit(X_train, y_train)
+        print "Train accuracy", model.score(X,y)
+        predicted = model.predict(X_test)
+        labels = set(predicted.tolist())
+        labels |= set(y_test.tolist())
+        sorted_headings = sorted(list(labels))
         confusion = confusion_matrix(y_test, predicted)
+        print sorted_headings
         print confusion
-        confusion_to_image(confusion, "confusion.png")
+        precision, recall = precision_recall_from_confusion(confusion)
+        print "Precision", precision
+        print "Recall", recall
+        #confusion_to_image(confusion, "confusion.png")
         sorted_labels = sorted(y_train.tolist())
-        score = test(clf, X_test, y_test, sorted_labels)
+        score = test(model, X_test, y_test, sorted_labels)
+        location_in_predicted(model, X_test, y_test)
         print score
